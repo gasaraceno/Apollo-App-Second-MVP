c2a082f8dee54183eb803dded4cc2eaaf6b9477b
Gabriele <gasaraceno12@gmail.com>
2018-05-01 11:36:10 -0400

    Apollo MVP 5/1/18

---
+++ apollo_mvp/apollo_mvp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apollo_mvp
+//
+//  Created by Zavien Anthony on 4/19/18.
+//  Copyright © 2018 apolloteam. All rights reserved.
+// Sergio, all the assets are in the assets.xcassets folder, Before you make any changes I am gonna try to push this to github I'll speak to gabriele ok GG EZ 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+}
+
